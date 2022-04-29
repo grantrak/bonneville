8d58269dacb4f55e6394f529f076dede7fbc47c9
Grant Rakowski <grakowski@bonneville.com>
2022-04-29 10:38:56 -0600

    finished mobile nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
+const header = document.querySelector('header');
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.getElementById('closeBtn');
 
+openBtn.addEventListener('click', () => {
+    header.classList.add('open');
+})
+
+closeBtn.addEventListener('click', () => {
+    header.classList.remove('open');
+})
 
